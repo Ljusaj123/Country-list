6a1f7297c1633e26c0cbe0fbd9187b3413271acf
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-21 20:07:22 +0200

    imported Search and Country list modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles/css/styles.css";
 import Header from "./modules/Header";
+import Search from "./modules/Search";
+import CountryList from "./modules/CountryList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search />
+      <CountryList />
     </div>
   );
 }
