54f22255b2a7f0d8546883c6653070593fb1f7b3
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-21 20:06:31 +0200

    cmodule for list of coutries on home pageć

---
+++ src/modules/CountryList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "../components/Card";
+import useFetch from "../hooks/useFetch";
+
+function CountryList() {
+  const { data, loading, error } = useFetch(
+    "https://restcountries.com/v3.1/region/oceania"
+  );
+
+  if (loading) {
+    return <p>Loading</p>;
+  }
+  if (error) {
+    return <p>error...</p>;
+  }
+
+  return (
+    <div className="country-list">
+      {data.map((country, index) => {
+        return <Card key={index} props={country} />;
+      })}
+    </div>
+  );
+}
+
+export default CountryList;
