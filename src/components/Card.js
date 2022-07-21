fc9af1e7b494eda72aca0607e2b987d327040aad
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-21 20:45:11 +0200

    added bold

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 const Card = ({ props }) => {
+  const { capital, region, population, name, flags } = props;
   return (
     <div className="card">
-      <img src={props.flags.png} alt="" />
-      <div className="coutry-info">
-        <h3>{props.name.common}</h3>
-        <p>Population: {props.population}</p>
-        <p>Region: {props.region}</p>
-        <p>Capital: {props.capital}</p>
+      <img src={flags.png} alt="" />
+      <div className="country-info">
+        <h3>{name.common}</h3>
+        <p>
+          <b>Population:</b> {population}
+        </p>
+        <p>
+          <b>Region:</b> {region}
+        </p>
+        <p>
+          <b>Capital:</b> {capital}
+        </p>
       </div>
     </div>
   );
