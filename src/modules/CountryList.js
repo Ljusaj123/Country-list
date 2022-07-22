7d1c35ce256c68059fc9a0df746d71f17ace30fd
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-22 18:02:46 +0200

    implemented loading component and error rendering

--- src/modules/CountryList.js
+++ src/modules/CountryList.js
@@ -2,17 +2,28 @@ import React from "react";
 import Card from "../components/Card";
 import useFetch from "../hooks/useFetch";
 
+import { DoubleBubble } from "react-spinner-animated";
+
+import "react-spinner-animated/dist/index.css";
+
 function CountryList() {
   const { data, loading, error } = useFetch(
     "https://restcountries.com/v3.1/region/oceania"
   );
 
   if (loading) {
-    return <p>Loading</p>;
+    return (
+      <DoubleBubble
+        text={"Loading..."}
+        bgColor={"#fafafa"}
+        width={"150px"}
+        height={"150px"}
+      />
+    );
   }
-  // if (error) {
-  //   return <p>error...</p>;
-  // }
+  if (error.isError) {
+    return <p>{error.message}</p>;
+  }
 
   return (
     <div className="country-list">
