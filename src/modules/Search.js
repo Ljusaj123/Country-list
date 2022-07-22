c7bc93d5805682656fe66d0ef60f99ecca2a08a7
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-07-22 18:03:39 +0200

    jsx for search module

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { regionList } from "../const/RegionList";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search-filter">
+      <div className="search-filter-container">
+        <input type="text" id="input" placeholder="Search for a country..." />
+
+        <div className="select">
+          <select name="format" id="format">
+            <option value="0" selected disabled>
+              Filter by Region:
+            </option>
+            {regionList.map((region, index) => {
+              return (
+                <option value={region} key={index}>
+                  {region}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
