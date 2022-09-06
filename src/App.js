cfa8e3bee210d23f7fc3910250fd1659c0987bf0
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-07 00:42:19 +0200

    imported css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Country from "./pages/Country";
 import Home from "./pages/Home";
+import "./styles/css/styles.css";
 function App() {
   return (
     <div className="App">
