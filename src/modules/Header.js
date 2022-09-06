e67924708181ea2314d6901d30787d9e97253050
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-07 00:06:35 +0200

    refactored code

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -4,9 +4,9 @@ import { BsFillMoonFill } from "react-icons/bs";
 function Header() {
   return (
     <header className="header">
-      <div className="header-container">
+      <div className="header__container container">
         <h1>Where in the world?</h1>
-        <div className="mode-colors">
+        <div className="header__mode-colors">
           <BsFillMoonFill />
           <p>Dark Mode</p>
         </div>
