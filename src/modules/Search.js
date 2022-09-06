bfb713d841689c567e1e5c28a21cdc5cedbc8ba9
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-09-07 00:29:18 +0200

    refactored code

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { regionList } from "../const/RegionList";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { regionList } from "../const/RegionList.js";
 import DataContext from "../contexts/DataContext";
 
 function Search() {
@@ -11,40 +10,36 @@ function Search() {
   };
 
   const handleInput = (query) => {
-    if (!query) {
-      setUrl("https://restcountries.com/v3.1/all");
-    } else {
+    if (query) {
       setUrl(`https://restcountries.com/v3.1/name/${query}`);
     }
   };
   return (
     <div className="search-filter">
-      <div className="search-filter-container">
+      <div className="search-filter__container container">
         <input
           type="text"
-          id="input"
+          className="search__input"
           placeholder="Search for a country..."
           onChange={(e) => handleInput(e.target.value)}
         />
-
-        <div className="select">
-          <select
-            name="format"
-            id="format"
-            onChange={(e) => handleClick(e.target.value)}
-          >
-            <option value="DEFAULT" selected disabled>
-              Filter by Region
-            </option>
-            {regionList.map((region, index) => {
-              return (
-                <option value={region} key={index}>
-                  {region}
-                </option>
-              );
-            })}
-          </select>
-        </div>
+        <select
+          name="format"
+          id="format"
+          className="search-filter__select"
+          onChange={(e) => handleClick(e.target.value)}
+        >
+          <option value="DEFAULT" selected disabled>
+            Filter by Region
+          </option>
+          {regionList.map((region, index) => {
+            return (
+              <option value={region} key={index}>
+                {region}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </div>
   );
